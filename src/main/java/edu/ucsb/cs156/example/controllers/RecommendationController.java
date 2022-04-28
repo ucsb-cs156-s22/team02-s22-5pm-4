bf3789fadd782d0f7c0fa83315ffb4750cf590d6
Kevin Zhang <kevinzhang425@gmail.com>
2022-04-27 19:30:19 -0700

    Added recommendation Controller. Added get all, get id, and post endpoints

---
+++ src/main/java/edu/ucsb/cs156/example/controllers/RecommendationController.java
@@ -0,0 +1,119 @@
+package edu.ucsb.cs156.example.controllers;
+
+import edu.ucsb.cs156.example.entities.Recommendation;
+import edu.ucsb.cs156.example.errors.EntityNotFoundException;
+import edu.ucsb.cs156.example.repositories.RecommendationRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import lombok.extern.slf4j.Slf4j;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+import java.time.LocalDateTime;
+
+@Api(description = "Recommendation")
+@RequestMapping("/api/Recommendation")
+@RestController
+@Slf4j
+public class RecommendationController extends ApiController {
+
+    @Autowired
+    RecommendationRepository recommendationRepository;
+
+    @ApiOperation(value = "List all recommendation's")
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @GetMapping("/all")
+    public Iterable<Recommendation> allRecommendations() {
+        Iterable<Recommendation> recommendations = recommendationRepository.findAll();
+        return recommendations;
+    }
+
+    @ApiOperation(value = "Get a single recommendation")
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @GetMapping("")
+    public Recommendation getById(
+            @ApiParam("id") @RequestParam Long id) {
+        Recommendation recommendation = recommendationRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException(Recommendation.class, id));
+
+        return recommendation;
+    }
+
+    @ApiOperation(value = "Create a new recommendation")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PostMapping("/post")
+    public Recommendation postUCSBDate(
+            @ApiParam("requesterEmail") @RequestParam String requesterEmail,
+            @ApiParam("professorEmail") @RequestParam String professorEmail,
+            @ApiParam("explanation") @RequestParam String explanation,
+            @ApiParam("date (in iso format, e.g. YYYY-mm-ddTHH:MM:SS; see https://en.wikipedia.org/wiki/ISO_8601)") @RequestParam("localDateTime") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dateRequested,
+            @ApiParam("date (in iso format, e.g. YYYY-mm-ddTHH:MM:SS; see https://en.wikipedia.org/wiki/ISO_8601)") @RequestParam("localDateTime") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dateNeeded,
+            @ApiParam("done") @RequestParam boolean done)
+            throws JsonProcessingException {
+
+        // For an explanation of @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+        // See: https://www.baeldung.com/spring-date-parameters
+
+        log.info("dateRequested={}", dateRequested);
+        log.info("dateNeeded={}", dateNeeded);
+
+        Recommendation recommendation = new Recommendation();
+        recommendation.setRequesterEmail(requesterEmail);
+        recommendation.setProfessorEmail(professorEmail);
+        recommendation.setExplanation(explanation);
+        recommendation.setDateRequested(dateRequested);
+        recommendation.setDateNeeded(dateNeeded);
+        recommendation.setDone(done);
+
+        Recommendation savedRecommendation = recommendationRepository.save(recommendation);
+
+        return savedRecommendation;
+    }
+
+    // @ApiOperation(value = "Delete a UCSBDate")
+    // @PreAuthorize("hasRole('ROLE_ADMIN')")
+    // @DeleteMapping("")
+    // public Object deleteUCSBDate(
+    //         @ApiParam("id") @RequestParam Long id) {
+    //     UCSBDate ucsbDate = ucsbDateRepository.findById(id)
+    //             .orElseThrow(() -> new EntityNotFoundException(UCSBDate.class, id));
+
+    //     ucsbDateRepository.delete(ucsbDate);
+    //     return genericMessage("UCSBDate with id %s deleted".formatted(id));
+    // }
+
+    // @ApiOperation(value = "Update a single date")
+    // @PreAuthorize("hasRole('ROLE_ADMIN')")
+    // @PutMapping("")
+    // public UCSBDate updateUCSBDate(
+    //         @ApiParam("id") @RequestParam Long id,
+    //         @RequestBody @Valid UCSBDate incoming) {
+
+    //     UCSBDate ucsbDate = ucsbDateRepository.findById(id)
+    //             .orElseThrow(() -> new EntityNotFoundException(UCSBDate.class, id));
+
+    //     ucsbDate.setQuarterYYYYQ(incoming.getQuarterYYYYQ());
+    //     ucsbDate.setName(incoming.getName());
+    //     ucsbDate.setLocalDateTime(incoming.getLocalDateTime());
+
+    //     ucsbDateRepository.save(ucsbDate);
+
+    //     return ucsbDate;
+    // }
+}
